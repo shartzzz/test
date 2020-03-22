153705da08c00ec5584867d455e7cd2c53c4b705
shartzzz <funnydist@gmail.com>
2020-03-22 21:49:28 +0300

    Updates

---
+++ precache-manifest.bba1e5b9975c07da04e0f36e08fdaa14.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9753f0b26b968b90ce8c5d2afbbbf99b",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "f84d41f4789189bf7cf5",
+    "url": "/test/static/css/2.3e9c613c.chunk.css"
+  },
+  {
+    "revision": "8baedb13d1a7e19796fe",
+    "url": "/test/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "f84d41f4789189bf7cf5",
+    "url": "/test/static/js/2.5679ce99.chunk.js"
+  },
+  {
+    "revision": "8baedb13d1a7e19796fe",
+    "url": "/test/static/js/main.560952e9.chunk.js"
+  },
+  {
+    "revision": "f93fd58490b200634951",
+    "url": "/test/static/js/runtime-main.48df6b48.js"
+  },
+  {
+    "revision": "500eb70d618d3125851fdf67914b2660",
+    "url": "/test/static/media/logo.500eb70d.svg"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/test/static/media/persik.4e1ec840.png"
+  }
+]);
